6dd592e0d5450c4d24a75701622d7bccfa60666e
Heejo You <hee_jo.you@uconn.edu>
2018-10-23 21:00:52 -0400

    Update Hyper_Parameters.py
    No synthesize in this repo

--- Hyper_Parameters.py
+++ Hyper_Parameters.py
@@ -20,34 +20,5 @@ speaker_Embedding_Parameters_Dict = {
     "extract_Path": "D:/Multi_Speaker_TTS_Result/Speaker_Embedding",
     }
 
-synthesizer_Parameters_Dict = {
-    "mel_Dimension": 80,
-    "batch_Size": 16,
-    "learning_Rate": 0.002,
-    "learning_Rate_Decay": "noam", #"none"
-    "max_Queue": 40,
-    "pattern_Sorting": True,
-    "token_Embedding_Size": 512,
-    "encoder_Conv_Filter_Count": 512,
-    "encoder_Conv_Kernel_Size": 5,
-    "encoder_Conv_Layer_Count": 3,
-    "encoder_LSTM_Cell_Size": 256,
-    "encoder_Zoneout_Rate": 0.1,
-    "attention_Size": 128,
-    "decoder_Prenet_Layer_Size": 256,
-    "decoder_Prenet_Layer_Count": 2,
-    "decoder_Prenet_Dropout_Rate": 0.5,    
-    "decoder_LSTM_Cell_Size": 1024,
-    "decoder_Zoneout_Rate": 0.1,
-    "decoder_Output_Size_per_Step": 1,
-    "decoder_Postnet_Conv_Filter_Count": 512,
-    "decoder_Postnet_Conv_Kernal_Size": 5,
-    "decoder_Postnet_Conv_Layer_Count": 5,
-    "decoder_Max_Mel_Length": 1400,
-    "pattern_Path": "D:/Simulation_Raw_Data/Multi_Speaker_TTS/Synthesizer",
-    "extract_Path": "D:/Multi_Speaker_TTS_Result/Synthesizer",
-    }
-
 sound_Parameters = tf.contrib.training.HParams(**sound_Parameters_Dict);
 speaker_Embedding_Parameters = tf.contrib.training.HParams(**speaker_Embedding_Parameters_Dict);
-synthesizer_Parameters = tf.contrib.training.HParams(**synthesizer_Parameters_Dict);
